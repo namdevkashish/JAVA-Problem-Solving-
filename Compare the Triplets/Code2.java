dcb25be06d93e8d512b63bce0a40f2afa2826655
namdevkashish <114773155+namdevkashish@users.noreply.github.com>
2022-10-26 15:52:02 +0530

    Click here to view question
    Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
    
    The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
    
    The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
    
    If a[i] > b[i], then Alice is awarded 1 point.
    If a[i] < b[i], then Bob is awarded 1 point.
    If a[i] = b[i], then neither person receives a point.
    Comparison points is the total points a person earned.
    
    Given a and b, determine their respective comparison points.
    
    Example
    
    a = [1, 2, 3]
    b = [3, 2, 1]
    For elements *0*, Bob is awarded a point because a[0] .
    For the equal elements a[1] and b[1], no points are earned.
    Finally, for elements 2, a[2] > b[2] so Alice receives a point.
    The return array is [1, 1] with Alice's score first and Bob's second.
    
    Function Description
    
    Complete the function compareTriplets in the editor below.
    
    compareTriplets has the following parameter(s):
    
    int a[3]: Alice's challenge rating
    int b[3]: Bob's challenge rating
    Return
    
    int[2]: Alice's score is in the first position, and Bob's score is in the second.
    Input Format
    
    The first line contains 3 space-separated integers, a[0], a[1], and a[2], the respective values in triplet a.
    The second line contains 3 space-separated integers, b[0], b[1], and b[2], the respective values in triplet b.
    
    Constraints
    
    1 ≤ a[i] ≤ 100
    1 ≤ b[i] ≤ 100
    Sample Input 0
    
    5 6 7
    3 6 10
    Sample Output 0
    
    1 1
    Explanation 0
    In this example:
    a=(a[0],a[1],a[2])=(5,6,7)
    b=(b[0],b[1],b[2])=(3,6,10)
    Now, let's compare each individual score:
    
    a[0]>b[0], so Alice receives 1 point.
    a[1]=b[1], so nobody receives a point.
    a[2]<B[2], so Bob receives 1 point.
    Alice's comparison score is 1, and Bob's comparison score is 1. Thus, we return the array [1,1].

---
+++ Compare the Triplets/Code2.java
@@ -0,0 +1,121 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'compareTriplets' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        List myList = new ArrayList();
+        int alice=0, bob=0, i=0, j=0;
+        
+        while(i < a.size() && j < b.size()) {
+        if (a.get(i) > b.get(j)) {
+            alice++;
+            i++;
+            j++;
+        }
+        else if (b.get(j) > a.get(i)) {
+            bob++;
+            i++;
+            j++;
+        }
+        else {
+            i++;
+            j++;
+        }
+    }
+
+    myList.add(alice);
+    myList.add(bob);
+    return myList;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] aTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> a = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            int aItem = Integer.parseInt(aTemp[i]);
+            a.add(aItem);
+        }
+
+        String[] bTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> b = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            int bItem = Integer.parseInt(bTemp[i]);
+            b.add(bItem);
+        }
+
+        List<Integer> result = Result.compareTriplets(a, b);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+/*
+#TEST CASE 1:
+Compiler Message
+Success
+
+Input (stdin)
+1 2 3
+1 2 3
+
+Expected Output
+0 0
+
+#TEST CASE 2:
+Compiler Messege
+Success
+
+Input (stdin)
+10 15 20
+5 6 7
+
+Expected Output
+3 0
+
+#TEST CASE 3:
+Compiler Messege
+Success
+
+Input (stdin)
+20 20 30
+20 20 50
+
+Expected Output
+0 1
+
+*/
